984c60e0b4d26c10b80262a5c76b63d23e2872ca
zhangqi <zh1402717363@sina.com>
2020-03-23 21:42:43 +0800

    修改发现音乐样式

--- pages/discover/index.js
+++ pages/discover/index.js
@@ -5,14 +5,23 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    currentTab: 0
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
 
+  },
+  clickTab: function (e) {
+    var that = this;
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        currentTab: e.target.dataset.current,
+      })
+    }
   },
 
   /**
